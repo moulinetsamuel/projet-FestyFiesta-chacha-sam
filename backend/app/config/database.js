aaaebd71be70de54283bd355c65ff6a13bf3fefa
moulinetsamuel <samuel.moulinet@oclock.school>
2024-02-29 10:54:30 +0100

    ✨feature: ajout d'une pool sur la connexion a la bdd

--- backend/app/config/database.js
+++ backend/app/config/database.js
@@ -3,6 +3,12 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, {
   dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
   define: {
     underscored: true,
   },
