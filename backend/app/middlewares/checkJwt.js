a2504cec51c5e337b73f0aefc45dcec6adedfd8a
moulinetsamuel <samuel.moulinet@oclock.school>
2024-03-20 16:24:55 +0100

    ✨feature: mise en place des jwt et securisation de toute les routes

---
+++ backend/app/middlewares/checkJwt.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+
+const extractToken = (authorization) => {
+  if (typeof authorization !== 'string') {
+    throw new Error('Authorization header missing or not a string');
+  }
+  const matches = authorization.match(/(Bearer)\s+(\S+)/i);
+  if (!matches) {
+    throw new Error('Bearer token not found in Authorization header');
+  }
+  return matches[2];
+};
+
+const checkJwt = (req, res, next) => {
+  try {
+    const token = req.headers.authorization && extractToken(req.headers.authorization);
+
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+      req.user = user;
+      console.log('JWT verified:', req.user);
+      next();
+    });
+  } catch (error) {
+    console.error('Error during JWT verification:', error.message);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export default checkJwt;
--- backend/app/routers/api/index.api.router.js
+++ backend/app/routers/api/index.api.router.js
@@ -3,12 +3,13 @@ import authRouter from './auth.api.router.js';
 import eventRouter from './event.api.router.js';
 import objectRouter from './object.api.router.js';
 import userRouter from './user.api.router.js';
+import  checkToken  from '../../middlewares/checkJwt.js';
 
 const router = express.Router();
 
-router.use('/event', eventRouter);
-router.use('/object', objectRouter);
-router.use('/user', userRouter);
+router.use('/event', checkToken, eventRouter);
+router.use('/object', checkToken, objectRouter);
+router.use('/user', checkToken, userRouter);
 router.use('/auth', authRouter);
 
 export default router;
